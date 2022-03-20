df89e849251087a323627e8688c8a4394101b68c
Nuclear One <ajithsnair10@gmail.com>
2022-03-20 21:23:38 +0530

    Add new features

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,18 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 3, 15);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
+  const LocationOfExpenditure = 'High Hill';
   return (
-    <div>
-      <ul>
-        <li>Food Rs 10 </li>
-        <li>Petrol Rs 100</li>
-        <li>Movies Rs 200</li>
-      </ul>
+    <div className="expense-item">
+      <div>Tme: {expenseDate.toISOString() } </div>
+      <div> //  Location:{LocationOfExpenditure} </div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle} </h2>
+        <div className="expense-item__price"> ${expenseAmount}</div>
+      </div>
     </div>
   );
 }
