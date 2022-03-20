8428e19abd5fb31e12980052ab00f1f9fd750beb
Nuclear One <ajithsnair10@gmail.com>
2022-03-20 23:38:24 +0530

    Add multiple expenses

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,15 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 3, 15);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 294.67;
-  const LocationOfExpenditure = 'High Hill';
+function ExpenseItem(props) {
+
+
   return (
     <div className="expense-item">
-      <div>Tme: {expenseDate.toISOString() } </div>
-      <div> //  Location:{LocationOfExpenditure} </div>
+      <div>Tme: {props.date.toISOString() } </div>
+      <div> //  Location:{props.location} </div>
       <div className="expense-item__description">
-        <h2>{expenseTitle} </h2>
-        <div className="expense-item__price"> ${expenseAmount}</div>
+        <h2>{props.title} </h2>
+        <div className="expense-item__price"> ${props.amount}</div>
       </div>
     </div>
   );
