2d508330c881a882eebc6e14c7a70ad57fc1f404
Nuclear One <ajithsnair10@gmail.com>
2022-03-20 18:55:18 +0530

    React App

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <ul>
+        <li>Food Rs 10 </li>
+        <li>Petrol Rs 100</li>
+        <li>Movies Rs 200</li>
+      </ul>
+    </div>
+  );
+}
+
+export default ExpenseItem;
