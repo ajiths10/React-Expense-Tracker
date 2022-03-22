def2dc88be879bc4465a5fa60f9cfdcad4d784d2
Nuclear One <ajithsnair10@gmail.com>
2022-03-22 16:47:02 +0530

    Add multiple useState

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,22 +1,39 @@
 import "./ExpenseForm.css";
+import React, { useState } from 'react';
 
 const ExpenseForm = () => {
+  const [enteredTitle,setEnteredTitle] = useState('');
+  const [enteredAmount,setEntertedAmount] = useState('');
+  const [enteredDate,setEntertedDate] = useState('');
+
+  const titleChangeHandler = (event) =>{
+    setEnteredTitle(event.target.value);
+  };
+
+  const amountChangeHandler = (event) =>{
+    setEntertedAmount(event.target.value)
+  };
+
+  const dateChangeHandler = (event) =>{
+    setEntertedDate(event.target.value)
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title :</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
 
         <div className="new-expense__control">
           <label>Amount :</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
         </div>
 
         <div className="new-expense__control">
           <label>Date :</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" />
+          <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
         </div>
 
       </div>
