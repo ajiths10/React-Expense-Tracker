cdca33d8db5192f99196922427518923d8b7c330
Nuclear One <ajithsnair10@gmail.com>
2022-03-22 14:26:48 +0530

    Add form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-
+import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
+
+
 
 const App = () => {
   const expenses = [
@@ -34,7 +36,7 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense/>
       <Expenses items={expenses} />
     </div>
   );
